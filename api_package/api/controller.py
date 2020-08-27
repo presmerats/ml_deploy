f0e4a9799374a4f13bb1294605d66de76833c4ea
Pau Rodriguez <rodriguez.pau@gmail.com>
2020-08-28 00:39:31 +0200

    Version endpoint in the API

---
+++ api_package/api/controller.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+from typeform_challenge.predict import make_prediction
+from typeform_challenge import __version__ as _version
+
+from api.config import get_logger
+from api import __version__ as api_version
+
+
+_logger = get_logger(logger_name=__name__)
+
+
+prediction_app = Blueprint("prediction_app", __name__)
+
+
+@prediction_app.route("/health", methods=["GET"])
+def health():
+    if request.method == "GET":
+        _logger.info("health status OK")
+        return "ok"
+
+
+@prediction_app.route("/version", methods=["GET"])
+def version():
+    if request.method == "GET":
+        return jsonify({"model_version": _version, "api_version": api_version})
+
+
+@prediction_app.route("/v1/predict/regression", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        json_data = request.get_json()
+        _logger.info(f"Inputs: {json_data}")
+
+        result = make_prediction(input_data=json_data)
+        _logger.info(f"Outputs: {result}")
+
+        predictions = result.get("predictions")[0]
+        version = result.get("version")
+
+        return jsonify({"predictions": predictions, "version": version})
