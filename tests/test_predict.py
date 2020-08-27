6347af1ece325cc2290ed14ae42d022da912fa74
Pau Rodriguez <rodriguez.pau@gmail.com>
2020-08-27 18:27:18 +0200

    validation of data

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -12,9 +12,22 @@ def test_make_single_prediction():
     # When
     subject = make_prediction(input_data=single_test_json)
 
-    print(round(subject.get("predictions")[0], 4))
-
     # Then
     assert subject is not None
     assert isinstance(subject.get("predictions")[0], float)
     assert round(subject.get("predictions")[0], 4) == 0.4865
+
+
+def test_make_multiple_predictions():
+    # Given
+    test_data = load_dataset(file_name="test.csv")
+    original_data_length = len(test_data)
+    multiple_test_json = test_data.to_json(orient="records")
+
+    # When
+    subject = make_prediction(input_data=multiple_test_json)
+
+    # Then
+    assert subject is not None
+    assert len(subject.get("predictions")) == 9
+
--- typeform_challenge/predict.py
+++ typeform_challenge/predict.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from typeform_challenge.processing.data_management import load_pipeline
 from typeform_challenge.config import config
+from typeform_challenge.processing.validation import validate_inputs
 
 
 pipeline_file_name = "typeform_challenge.pkl"
@@ -12,6 +13,7 @@ def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
     data = pd.read_json(input_data)
+    validated_data = validate_inputs(input_data=data)
     prediction = _typeform_pipe.predict(data[config.FEATURES])
     # inverse_transform (empty for now)
     output = prediction
