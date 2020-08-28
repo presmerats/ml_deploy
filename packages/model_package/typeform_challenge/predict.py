e14ff41efa86b4011e64aee3e60fc0e75b2c16cd
Pau Rodriguez <rodriguez.pau@gmail.com>
2020-08-28 15:01:10 +0200

    input_data on predict.py

--- packages/model_package/typeform_challenge/predict.py
+++ packages/model_package/typeform_challenge/predict.py
@@ -20,7 +20,7 @@ def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
     # data = pd.read_json(input_data)
-    _logger.info(f"input_data: {input_data}")
+    _logger.info(f"input_data: {input_data}" f"type: {type(input_data)}")
 
     data = pd.DataFrame(input_data)
     validated_data = validate_inputs(input_data=data)
