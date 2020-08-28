a1455e65f99278b65777f7196006f4f508492602
Pau Rodriguez <rodriguez.pau@gmail.com>
2020-08-28 14:45:21 +0200

    updateing version for the package to get published

--- packages/model_package/typeform_challenge/predict.py
+++ packages/model_package/typeform_challenge/predict.py
@@ -20,9 +20,8 @@ def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
     # data = pd.read_json(input_data)
-    _logger.info(f"iput_data: {input_data}")
+    _logger.info(f"input_data: {input_data}")
 
-    input_data = json.loads(input_data)
     data = pd.DataFrame(input_data)
     validated_data = validate_inputs(input_data=data)
     prediction = _typeform_pipe.predict(data[config.FEATURES])
